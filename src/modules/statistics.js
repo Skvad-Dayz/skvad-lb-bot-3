17dcc76a928b7e9bd7b031b7546a57a18ef900a6
Tobias Reichel <frankblack@skvad.network>
2023-05-26 01:39:32 +0200

    Typo

--- src/modules/statistics.js
+++ src/modules/statistics.js
@@ -6,7 +6,7 @@ const { createHitZonesHeatMap } = require('./heatmap');
 const { AttachmentBuilder } = require('discord.js');
 
 const playerStatisticsCtx = async (cfg, {
-  //names;
+  names,
   playtime,
   sessions,
   statistics: {
@@ -38,7 +38,7 @@ const playerStatisticsCtx = async (cfg, {
   const cleanedWeaponName = titleCase(favoriteWeaponName.replace(/_/g, ' '));
 
   // Reversing the name history array so the latest used name is the first item
-  //names.reverse();
+  names.reverse();
 
   // Generate hit zone image
   const files = [];
